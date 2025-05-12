a4b021877b35f54709fb3df055103298e96b1208
Craig Mrock <craigcmrock@gmail.com>
2025-05-12 11:37:02 -0600

    first commit

---
+++ guitar.js
@@ -0,0 +1,215 @@
+import { scalesData } from './scalesData.js';
+
+(function() {
+    const keysButtons = new Array();
+  keysButtons[0] = document.querySelector('.cKey-btn');
+  keysButtons[1] = document.querySelector('.fKey-btn');
+  keysButtons[2] = document.querySelector('.bbKey-btn');
+  keysButtons[3] = document.querySelector('.ebKey-btn');
+  keysButtons[4] = document.querySelector('.abKey-btn');
+  keysButtons[5] = document.querySelector('.dbKey-btn');
+  keysButtons[6] = document.querySelector('.gKey-btn');
+  keysButtons[7] = document.querySelector('.dKey-btn');
+  keysButtons[8] = document.querySelector('.aKey-btn');
+  keysButtons[9] = document.querySelector('.eKey-btn');
+  keysButtons[10] = document.querySelector('.bKey-btn');
+  keysButtons[11] = document.querySelector('.fsKey-btn');
+  keysButtons[12] = document.querySelector('.csKey-btn');
+  
+  const scaleText = document.querySelector('.scale-results');
+  const root = document.documentElement;
+  const fretboard = document.querySelector('.fretboard1');
+  const singleFretMarkPositions = [3, 5, 7, 9, 15, 17, 19, 21];
+  const doubleFretMarkPositions = [12, 24];
+  const numberOfFrets = 24;
+  const notesFlat = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"];
+  const notesSharp = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+  const instrumentTuningPresets = {
+    'Guitar (6 strings)': [4, 11, 7, 2, 9, 4]
+  };
+  
+  let accidentals = 'flats';
+  let selectedInstrument = 'Guitar (6 strings)';
+  let numberOfStrings = instrumentTuningPresets[selectedInstrument].length;
+  
+  let scaleResultsString = '';
+  
+  
+  function showTypeBtns () {
+    document.getElementById("type-btn").style.display = "block";
+  }
+  
+  function setupFretboard(scaleNotes) {
+    console.log('Generating fretboard with scale notes:', scaleNotes); // Debugging
+  
+    fretboard.innerHTML = ''; // Clear existing fretboard
+    root.style.setProperty('--number-of-strings', numberOfStrings);
+  
+    // Add fret number row
+    const fretNumberRow = document.createElement('div');
+    fretNumberRow.classList.add('fret-number-row');
+    for (let fret = 0; fret <= numberOfFrets; fret++) {
+        const fretNumber = document.createElement('div');
+        fretNumber.classList.add('fret-number');
+        fretNumber.textContent = fret === 0 ? 'Open' : fret; // Leave the first column empty for the nut
+        fretNumberRow.appendChild(fretNumber);
+    }
+    fretboard.appendChild(fretNumberRow);
+  
+    // Add strings to the fretboard
+    for (let i = 0; i < numberOfStrings; i++) {
+      const string = createStringElement();
+      fretboard.appendChild(string);
+  
+      for (let fret = 0; fret <= numberOfFrets; fret++) {
+          const noteFret = createNoteFretElement(i, fret, scaleNotes);
+          string.appendChild(noteFret);
+      }
+  }
+  
+  /**
+   * Creates a string element for the fretboard.
+   * @returns {HTMLElement} The string element.
+   */
+  function createStringElement() {
+      const string = document.createElement('div');
+      string.classList.add('sg-string');
+      return string;
+  }
+  
+  /**
+   * Creates a note fret element for a specific string and fret.
+   * @param {number} stringIndex - The index of the string.
+   * @param {number} fret - The fret number.
+   * @param {Array} scaleNotes - The notes in the selected scale.
+   * @returns {HTMLElement} The note fret element.
+   */
+  function createNoteFretElement(stringIndex, fret, scaleNotes) {
+      const noteFret = document.createElement('div');
+      noteFret.classList.add('sg-note-fret');
+  
+      const noteName = generateNoteNames(fret + instrumentTuningPresets[selectedInstrument][stringIndex], accidentals);
+      noteFret.setAttribute('data-note', noteName);
+  
+      // Add single fret marks
+      if (stringIndex === 0 && singleFretMarkPositions.includes(fret)) {
+          noteFret.classList.add('sg-single-fretmark');
+      }
+  
+      // Add double fret marks
+      if (stringIndex === 0 && doubleFretMarkPositions.includes(fret)) {
+          const doubleFretMark = tools.createElement('div');
+          doubleFretMark.classList.add('sg-double-fretmark');
+          noteFret.appendChild(doubleFretMark);
+      }
+  
+      // Highlight notes in the scale
+      if (scaleNotes.includes(noteName)) {
+          highlightNoteFret(noteFret, noteName, scaleNotes);
+      }
+  
+      return noteFret;
+  }
+  
+  /**
+   * Highlights a note fret element based on its role in the scale.
+   * @param {HTMLElement} noteFret - The note fret element.
+   * @param {string} noteName - The name of the note.
+   * @param {Array} scaleNotes - The notes in the selected scale.
+   */
+  function highlightNoteFret(noteFret, noteName, scaleNotes) {
+      noteFret.classList.add('highlighted');
+      noteFret.style.setProperty('--noteDotOpacity', 1); // Explicitly set the opacity
+  
+      /* // Set background color for the root note
+      if (noteName.trim().toLowerCase() === scaleNotes[0].trim().toLowerCase()) {
+          noteFret.style.backgroundColor = '#61eb34'; // Green background for the root note
+      }
+  
+      // Set background color for the third note in the scale
+      if (scaleNotes.length >= 7 && noteName.trim().toLowerCase() === scaleNotes[2].trim().toLowerCase()) {
+          noteFret.style.backgroundColor = '#d142f5'; // Purple background for the third note
+      } */
+  }
+  }
+  
+  const tools = {
+    createElement(element, content) {
+        element = document.createElement(element);
+        if (arguments.length > 1) {
+            element.innerHTML = content;
+        }
+        return element;
+  
+    }
+  }
+  
+  function generateNoteNames(noteIndex, accidentals) {
+    noteIndex = noteIndex % 12;
+  
+    // Check if scaleResultsString contains sharp or flat notes
+    if (scaleResultsString.includes('#')) {
+      console.log("Scale Results String contains sharp notes:", scaleResultsString); // Debugging
+        accidentals = 'sharps'; // Update accidentals to sharps
+    } else if (scaleResultsString.includes('b')) {
+      console.log("Scale Results String contains flat notes:", scaleResultsString); // Debugging
+        accidentals = 'flats'; // Update accidentals to flats
+    }
+  
+    // Return the appropriate note based on accidentals
+    return accidentals === 'sharps' ? notesSharp[noteIndex] : notesFlat[noteIndex];
+  }
+  
+  keysButtons.forEach((button, index) => {
+    const { major, minor } = scalesData[index];
+  
+    button.addEventListener('click', () => {
+        showTypeBtns();
+        setupFretboard([]); // Clear the fretboard before displaying new notes
+  
+        // Update the message and clear scale results
+        updateMessage(scalesData[index].key);
+  
+        // Add event listeners for major and minor buttons
+        const majBtn = document.querySelector('.major-btn');
+        const minBtn = document.querySelector('.minor-btn');
+
+        if (!majBtn || !minBtn) {
+          console.error("Major or Minor buttons are missing.");
+          return;
+      }
+  
+        majBtn.addEventListener('click', () => handleScaleSelection(major));
+        minBtn.addEventListener('click', () => handleScaleSelection(minor));
+    });
+  });
+  
+  /**
+  * Updates the message element and clears the scale results.
+  * @param {string} key - The selected key.
+  */
+  function updateMessage(key) {
+    const messageElement = document.getElementById("message");
+    messageElement.textContent = `You selected the key of: ${key}`;
+    scaleText.textContent = ''; // Clear the scale-results element
+  }
+  
+  /**
+  * Handles the selection of a scale (major or minor).
+  * @param {Array} scaleArray - The array of scales (major or minor).
+  */
+  function handleScaleSelection(scaleArray) {
+    const scaleResultsElement = document.getElementById("scale-results");
+    scaleResultsElement.style.display = "block";
+  
+    // Select a random scale and update scaleResultsString
+    scaleResultsString = scaleArray[Math.floor(Math.random() * scaleArray.length)];
+    scaleText.textContent = scaleResultsString;
+    console.log("Selected Scale:", scaleResultsString);
+  
+    // Extract notes from scaleResultsString and update the fretboard
+    const scaleNotes = scaleResultsString.split(': ')[1].split(' - ');
+    setupFretboard(scaleNotes);
+  };
+  
+  })();
---
+++ scalesData.js
@@ -0,0 +1,67 @@
+export const scalesData = [
+    {
+        key: 'C',
+        major: ['C Ionian: C - D - E - F - G - A - B', 'C Lydian: C - D - E - F# - G - A - B', 'C Mixolydian: C - D - E - F - G - A - Bb', 'C Maj Pentatonic: C - D - E - G - A'],
+        minor: ['C Dorian: C - D - Eb - F - G - A - Bb', 'C Phrygian: C - Db - Eb - F - G - Ab - Bb', 'C Aeolian: C - D - Eb - F - G - Ab - Bb', 'C Locrian: C - Db - Eb - F - Gb - Ab - Bb', 'C Melodic Minor: C - D - Eb - F - G - A - B', 'C Min Pentatonic: C - Eb - F - G - Bb', 'C Harmonic Minor: C - D - Eb - F - G - Ab - B']
+    },
+    {
+        key: 'F',
+        major: ['F Ionian: F - G - A - Bb - C - D - E', 'F Lydian: F - G - A - B - C - D - E', 'F Mixolydian: F - G - A - Bb - C - D - Eb', 'F Maj Pentatonic: F - G - A - C - D'],
+        minor: ['F Dorian: F - G - Ab - Bb - C - D - Eb', 'F Phrygian: F - Gb - Ab - Bb - C - Db - Eb', 'F Aeolian: F - G - Ab - Bb - C - Db - Eb', 'F Locrian: F - Gb - Ab - Bb - Cb - Db - Eb', 'F Melodic Minor: F - G - Ab - Bb - C - D - E', 'F Min Pentatonic: F - Ab - Bb - C - Eb', 'F Harmonic Minor: F - G - Ab - Bb - C - Db - E']
+    },
+    {
+      key: 'Bb',
+      major: ['Bb Ionian: Bb - C - D - Eb - F - G - A', 'Bb Lydian: Bb - C - D - E - F - G - A', 'Bb Mixolydian: Bb - C - D - Eb - F - G - Ab', 'Bb Maj Pentatonic: Bb - C - D - F - G'],
+      minor: ['Bb Dorian: Bb - C - Db - Eb - F - G - Ab', 'Bb Phrygian: Bb - Cb - Db - Eb - F - Gb - Ab', 'Bb Aeolian: Bb - C - Db - Eb - F - Gb - Ab', 'Bb Locrian: Bb - B - Db - Eb - E - Gb - Ab', 'Bb Melodic Minor: Bb - C - Db - Eb - F - G - A', 'Bb Min Pentatonic: Bb - Db - Eb - F - Ab', 'Bb Harmonic Minor: Bb - C - Db -Eb - F - Gb - A']
+    },
+    {
+      key: 'Eb',
+      major: ['Eb Ionian: Eb - F - G - Ab - Bb - C - D', 'Eb Lydian: Eb - F - G - A - Bb - C - D', 'Eb Mixolydian: Eb - F - G - A - Bb - C - D', 'Eb Maj Pentatonic: Eb - F - G - Bb - C'],
+      minor: ['Eb Dorian: Eb - F - Gb - Ab - Bb - C - Db', 'Eb Phrygian: Eb - Fb - Gb - Ab - Bb - Cb - Db', 'Eb Aeolian: Eb - F - Gb - Ab - Bb - Cb - Db', 'Eb Locrian: Eb - Fb - Gb - Ab - Bbb - Cb - Db', 'Eb Melodic Minor: Eb - F - Gb - Ab - Bb - C - D', 'Eb Min Pentatonic: Eb - Gb - Ab - Bb - Db', 'Eb Harmonic Minor: Eb - F - Gb - Ab - Bb - Cb - D']
+  },
+  {
+      key: 'Ab',
+      major: ['Ab Ionian: Ab - Bb - C - Db - Eb - F - G', 'Ab Lydian: Ab - Bb - C - D - Eb - F - G', 'Ab Mixolydian: Ab - Bb - C - Db - Eb - F - Gb', 'Ab Maj Pentatonic: Ab - Bb - C - Eb - F'],
+      minor: ['Ab Dorian: Ab - Bb - Cb - Db - Eb - F - Gb', 'Ab Phrygian: Ab - Bbb - Cb - Db - Eb - Fb - Gb', 'Ab Aeolian: Ab - Bb - Cb - Db - Eb - Fb - Gb', 'Ab Locrian: Ab - Bbb - Cb - Db - Ebb - Fb - Gb', 'Ab Melodic Minor: Ab - Bb - Cb - Db - Eb - F - G', 'G# Min Pentatonic: G# - B - C# - D# - F#', 'Ab Harmonic Minor: Ab - Bb - Cb - Db - Eb - Fb - G']
+  },
+  {
+    key: 'Db',
+    major: ['Db Ionian: Db - Eb - F - Gb - Ab - Bb - C', 'Db Lydian: Db - Eb - F - G - Ab - Bb - C', 'Db Mixolydian: Db - Eb - F - Gb - Ab - Bb - Cb', 'Db Maj Pentatonic: Db - Eb - F - Ab - Bb'],
+    minor: ['Db Dorian: Db - Eb - Fb - Gb - Ab - Bb - Cb', 'Db Phrygian: Db - Ebb - Fb - Gb - Ab - Bbb - Cb', 'Db Aeolian: Db - Eb - Fb - Gb - Ab - Bbb - Cb', 'Db Locrian: Db - Ebb - Fb - Gb - Abb - Bbb - Cb', 'Db Melodic Minor: Db - Eb - Fb - Gb - Ab - Bb - C', 'Db Min Pentatonic: Db - Fb - Gb - Ab - Cb', 'Db Harmonic Minor: Db - Eb - Fb - Gb - Ab - Bbb - C']
+  },
+  {
+    key: 'G',
+    major: ['G Ionian: G - A - B - C - D - E - F#', 'G Lydian: G - A - B - C# - D - E - F#', 'G Mixolydian: G - A - B - C - D - E - F', 'G Maj Pentatonic: G - A - B - D - E'],
+    minor: ['G Dorian: G - A - Bb - C - D - E - F', 'G Phrygian: G - Ab - Bb - C - D - Eb - F', 'G Aeolian: G - A - Bb - C - D - Eb - F', 'G Locrian: G - Ab - Bb - C - Db - Eb - F', 'G Melodic Minor: G - A - Bb - C - D - E - F#', 'G Min Pentatonic: G - Bb - C - D - F', 'G Harmonic Minor: G - A - Bb - C - D - Eb - F#']
+  },
+  {
+    key: 'D',
+    major: ['D Ionian: D - E - F# - G - A - B - C#', 'D Lydian: D - E - F# - G# - A - B - C#', 'D Mixolydian: D - E - F# - G - A - B - C', 'D Maj Pentatonic: D - E - F# - A - B'],
+    minor: ['D Dorian: D - E - F - G  - A - B - C - D', 'D Phrygian: D - Eb - F - G - A - Bb - C', 'D Aeolian: D - E - F - G - A - Bb - C', 'D Locrian: D - Eb - F - G - Ab - Bb - C', 'D Melodic Minor: D - E - F - G - A - B - C#', 'D Min Pentatonic: D - F - G - A - C', 'D Harmonic Minor: D - E - F - G - A - Bb - C#']
+  },
+  {
+    key: 'A',
+    major: ['A Ionian: A - B - C# - D - E - F# - G#', 'A Lydian: A - B - C#- D# - E - F#- G#', 'A Mixolydian: A - B - C# - D - E - F# - G', 'A Maj Pentatonic: A - B - C# - E - F#'],
+    minor: ['A Dorian: A - B - C - D - E - F# - G', 'A Phrygian: A - Bb - C - D - E - F - G', 'A Aeolian: A - B - C - D - E - F - G', 'A Locrian: A - Bb - C - D - Eb - F - G', 'A Melodic Minor: A - B - C - D - E - F# - G#', 'A Min Pentatonic: A - C - D - E - G', 'A Harmonic Minor: A - B - C - D - E - F - G#']
+  },
+  {
+    key: 'E',
+    major: ['E Ionian: E - F# - G# - A - B - C# - D#', 'E Lydian: E - F# - G# - A# - B - C# - D#', 'E Mixolydian: E - F# - G# - A - B - C# - D', 'E Maj Pentatonic: E - F# - G# - B - C#'],
+    minor: ['E Dorian: E - F# - G - A - B - C# - D', 'E Phrygian: E - F - G - A - B - C - D', 'E Aeolian: E - F# - G - A - B - C - D', 'E Locrian: E - F - G - A - Bb - C - D', 'E Melodic Minor: E - F# - G - A - B - C# - D#', 'E Min Pentatonic: E - G - A - B - D', 'E Harmonic Minor: E - F# - G - A - B - C - D#']
+  },
+  {
+    key: 'B',
+    major: ['B Ionian: B - C# - D# - E - F# - G# - A#', 'B Lydian: B - C# - D#- E# - F# - G#- A#', 'B Mixolydian: B - C# - D# - E - F# - G# - A', 'B Maj Pentatonic: B - C# - D# - F# - G#'],
+    minor: ['B Dorian: B - C# - D - E - F# - G# - A', 'B Phrygian: B - C - D - E - F# - G - A', 'B Aeolian: B - C# - D - E - F# - G - A', 'B Locrian: B - C - D - E - F - G - A', 'B Melodic Minor: B - C# - D - E - F# - G# - A#', 'B Min Pentatonic: B - D - E - F# - A', 'B Harmonic Minor: B - C# - D - E - F# - G - A#']
+  },
+  {
+    key: 'F#',
+    major: ['F# Ionian: F# - G# - A# - B - C# - D# - E#', 'F# Lydian: F# - G# - A# - B# - C# - D# - E#', 'F# Mixolydian: F# - G# - A# - B - C# - D# - E', 'F# Maj Pentatonic: F# - G# - A# - C# - D#'],
+    minor: ['F# Dorian: F# - G# - A - B - C# - D# - E', 'F# Phrygian: F# - G - A - B - C# - D - E', 'F# Aeolian: F# - G# - A - B - C# - D - E', 'F# Locrian: F# - G - A - B - C - D - E', 'F# Melodic Minor: F# - G# - A - B - C# - D# - E#', 'F# Min Pentatonic: F# - A - B - C# - E', 'F# Harmonic Minor: F# - G# - A - B - C# - D - E#']
+  },
+  {
+    key: 'C#',
+    major: ['C# Ionian: C# - D# - E# - F# - G# - A# - B#', 'C# Lydian: C# - D# - E# - F## - G# - A# - B#', 'C# Mixolydian: C# - D# - E# - F# - G# - A# - B', 'C# Maj Pentatonic: C# - D# - E# - G# - A#'],
+    minor: ['C# Dorian: C# - D# - E - F# - G# - A# - B', 'C# Phrygian: C# - D - E - F# - G# - A - B', 'C# Aeolian: C# - D# - E - F# - G# - A - B', 'C# Locrian: C# - D - E - F# - G - A - B', 'C# Melodic Minor: C# - D# -E - F# - G# - A# - B#', 'C# Min Pentatonic: C# - E - F# - G# - B', 'C# Harmonic Minor: C# - D# - E - F# - G# - A - B#']
+  },
+  ];
